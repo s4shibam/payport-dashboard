d0c16922fcccc4f561721f120aa433953736c050
s4shibam <s4shibam@gmail.com>
2026-06-23 23:19:07 +0530

    chore: initialize next app with eslint, prettier and shadcn

---
+++ README.md
@@ -0,0 +1,3 @@
+# Payport - Real-Time Payments Dashboard
+
+A real-time payments dashboard for Payport, a global cross-border payments platform.
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-2">
+      <p className="text-2xl font-bold">Hello World</p>
+      <Button>Button</Button>
+    </div>
+  )
+}
+
+export default Home
